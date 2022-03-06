daa84c81c32df9f97f5e9d9c10ddf18d0e357abb
Jelle Meeus <jellemeeus@hotmail.com>
2022-03-06 17:15:31 +0100

    Change pdf links to online versions

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,6 +1,6 @@
 module.exports = {
-  pdfFileName_EN:  `CV_Jelle_Meeus_EN.pdf`,
-  pdfFileName_NL: `CV_Jelle_Meeus_NL.pdf`,
+  pdfLocation_EN:  `https://github.com/jellemeeus/resume-CV-pdf/raw/main/EN.pdf`,
+  pdfLocation_NL:  `https://github.com/jellemeeus/resume-CV-pdf/raw/main/NL.pdf`,
   name: 'Ing. Jelle Meeus',
   title: 'Software Developer',
   pdfName: 'jelle-meeus.software-developer.pdf',
